fb82229480ae4510ea07a5944a5f22244d559c8e
Gonz0o11 <Gonz0o101010@gmail.com>
2024-11-22 14:37:44 +0800

    First

---
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lab8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyService.class);
+                startService(intent);
+                Toast.makeText(MainActivity.this, "啟動Service", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        } );
+    }
+}
